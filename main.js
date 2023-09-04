974da809afd691024d6583e8c3682b1c31149881
Agustin123ccc <agustinreichel351@gmail.com>
2023-09-04 03:53:47 -0300

    tercer commit

--- main.js
+++ main.js
@@ -1,136 +1,169 @@
-let nombre = prompt("Bienvenido a nuestra pagina WEB! Aca podras comprar la casa de juguete ideal para tu niño/a. ¿Cual es su nombre?"); 
-alert("Un gusto " + nombre + "! gracias por confiar en nosotros, a continuacion te preguntaremos la edad del niño.");
-alert("¡Atencion, la ventas de las casas de juguetes estan disponibles para menores de maximo hasta 12 años de edad");
+document.addEventListener("DOMContentLoaded", function () {
+    const nombreInput = document.getElementById("nombreInput");
+    const nombreBtn = document.getElementById("nombreBtn");
+    const edadSection = document.getElementById("edadSection");
+    const edadInput = document.getElementById("edadInput");
+    const edadBtn = document.getElementById("edadBtn");
+    const direccionSection = document.getElementById("direccionSection");
+    const direccionInput = document.getElementById("direccionInput");
+    const direccionBtn = document.getElementById("direccionBtn");
+    const telefonoSection = document.getElementById("telefonoSection");
+    const telefonoInput = document.getElementById("telefonoInput");
+    const telefonoBtn = document.getElementById("telefonoBtn");
+    const productosSection = document.getElementById("productosSection");
+    const productoSelect = document.getElementById("productoSelect");
+    const productoBtn = document.getElementById("productoBtn");
+    const resultado = document.getElementById("resultado");
+    const productoInfo = document.getElementById("productoInfo");
+    const productoPrecio = document.getElementById("productoPrecio");
+    const comprarBtn = document.getElementById("comprarBtn");
+    const continuarComprandoBtn = document.getElementById("continuarComprandoBtn");
+    const compraSection = document.getElementById("compraSection");
+    const comprasInfo = document.getElementById("comprasInfo");
+    const totalGasto = document.getElementById("totalGasto");
+    const datosComprador = document.getElementById("datosComprador");
+    const nombreError = document.getElementById("nombreError");
+    const edadError = document.getElementById("edadError");
+    const direccionError = document.getElementById("direccionError");
+    const telefonoError = document.getElementById("telefonoError");
+    const productoError = document.getElementById("productoError");
 
- let edad = prompt("Ingrese la edad del niño:");
+    const APP_PREFIX = "juguetes_";
 
- while(edad > 12){
-    alert("Ups! ocurrio un problema... su hijo ya supera el limite de edad propuesto para comprar los juguetes.");
-    edad = prompt("Ingrese la edad del niño:");
-  }
+    let nombre = "";
+    let edad = 0;
+    let direccion = "";
+    let telefono = "";
+    let selectedProduct = null;
+    let totalCost = 0;
+    let compras = [];
+    let compradorData = {};
 
-  alert("¿Que tamaño de casa estas interesado comprar? te dejo los precios para que pienses y observes! "); 
-  alert("Para ver mas informacion de las casas u otras, coloca `Casa grande`, `Casa mediana`, `Casa pequeña` `Pack juguetes`")
+    const storedNombre = localStorage.getItem(APP_PREFIX + "nombre");
+    const storedDireccion = localStorage.getItem(APP_PREFIX + "direccion");
+    const storedTelefono = localStorage.getItem(APP_PREFIX + "telefono");
 
-const productos = [
-  { id: 1, nombrejuguete: "Casa grande", precio: 7500 },
-  { id: 2, nombrejuguete: "Casa mediana", precio: 4500 },
-  { id: 3, nombrejuguete: "Casa pequeña", precio: 2000 },
-  { id: 4, nombrejuguete: "Pack juguetes", precio: 500 },
-];
-
-let continuar = true;
-
-while (continuar) {
-  let nombrejuguete = prompt("Ingrese el nombre de la casa o juguete a consultar");
-  let producto;
-
-  for (const item of productos) {
-    if (item.nombrejuguete === nombrejuguete) {
-    producto = item;
+    if (storedNombre) {
+        nombreInput.value = storedNombre;
+        direccionInput.value = storedDireccion;
+        telefonoInput.value = storedTelefono;
     }
-  }
 
-  if (producto) {
-    let mensaje = `
-    ID: ${producto.id}
-   Nombre: ${producto.nombrejuguete}
-    $${producto.precio}
-    `;
-
-    alert(mensaje);
-  } else {
-    alert("La casa o juguete no se ha encontrado en la lista");
-  }
-
-  productos.sort((a, b) => b.precio - a.precio);
-
-alert("A continuación, te mostramos las casas u otros productos ordenados por precio de mayor a menor:");
-for (const producto of productos) {
-  alert(`
-    ID: ${producto.id}
-    Nombre: ${producto.nombrejuguete}
-    Precio: $${producto.precio}
-  `);
-}
-
-productos.sort((a, b) => a.precio - b.precio);
-
-alert("A continuación, te mostramos las casas u otros productos por precio de menor a mayor:");
-for (const producto of productos) {
-  alert(`
-    ID: ${producto.id}
-    Nombre: ${producto.nombrejuguete}
-    Precio: $${producto.precio}
-  `);
-}
-
-  let respuesta1 = prompt("¿Desea consultar otra casa u objeto? (si/no)");
-  if (respuesta1.toLowerCase() !== "si") {
-    continuar = false;
-  }
-}
-
-function juguete(selector) {
-    switch (selector) {
-        case "1":
-            return { nombre: "Casa Grande", precio: 7500 };
-        case "2":
-            return { nombre: "Casa Mediana", precio: 4500 };
-        case "3":
-            return { nombre: "Casa Pequeña", precio: 2000 };
-        case "4":
-            return { nombre: "Pack de Juguetes", precio: 500 };
-        default:
+    nombreBtn.addEventListener("click", function () {
+        nombre = nombreInput.value;
+        if (nombre.trim() === "") {
+            nombreError.textContent = "Por favor, ingresa tu nombre.";
             return;
-    }
-}
-
-function obtenerDatosComprador() {
-    let nombre = prompt("Ingrese su nombre:");
-    let direccion = prompt("Ingrese su dirección:");
-    let telefono = prompt("Ingrese su número de teléfono:");
-
-    return {
-        nombre: nombre,
-        direccion: direccion,
-        telefono: telefono
-    };
-}
-
-let seguirComprando = true;
-let totalGasto = 0;
-let compras = [];
-
-while (seguirComprando) {
-    alert("¿Qué tal? ¡Ya estamos por finalizar tu compra! Selecciona el que más te interesó para tu niño/a.");
-    let selector = prompt("Si estás interesado en elegir la casa más grande, coloca '1'. Si, en cambio, estás interesado en la casa mediana, coloca '2'. Si te interesa algo más pequeño, coloca '3'. Por último, si deseas comprar el pack de juguetes, coloca '4'.");
-
-    let producto = juguete(selector);
-    if (producto !== null) {
-        alert(`Usted escogió ${producto.nombre} por $${producto.precio} para su niño/a. ¡Gracias por su compra!`);
-        totalGasto += producto.precio;
-        compras.push(producto);
-
-        let respuesta = prompt("¿Desea comprar otro juguete? (si/no)");
-        if (respuesta.toLowerCase() !== "si") {
-            seguirComprando = false;
         }
-    } else {
-        alert("Juguete no encontrado");
+        nombreError.textContent = "";
+        edadSection.style.display = "block";
+        nombreInput.disabled = true;
+    });
+
+    edadBtn.addEventListener("click", function () {
+        edad = parseInt(edadInput.value);
+        if (isNaN(edad) || edad <= 0) {
+        edadError.textContent = "Por favor, ingresa una edad válida.";
+        return;
+        }
+        if (edad > 12) {
+        edadError.textContent = "Lo siento, esta tienda está disponible solo para niños menores de 12 años.";
+        return;
+        }
+        edadError.textContent = "";
+        direccionSection.style.display = "block";
+        edadInput.disabled = true;
+    });
+
+    direccionBtn.addEventListener("click", function () {
+        direccion = direccionInput.value;
+        if (direccion.trim() === "") {
+        direccionError.textContent = "Por favor, ingresa tu dirección.";
+            return;
+        }
+        direccionError.textContent = "";
+        telefonoSection.style.display = "block";
+        direccionInput.disabled = true;
+    });
+
+    telefonoBtn.addEventListener("click", function () {
+        telefono = telefonoInput.value;
+        if (telefono.trim() === "") {
+            telefonoError.textContent = "Por favor, ingresa tu número de teléfono.";
+            return;
+        }
+        telefonoError.textContent = "";
+        productosSection.style.display = "block";
+        telefonoInput.disabled = true;
+    });
+
+    productoBtn.addEventListener("click", function () {
+        const selectedProductName = productoSelect.value;
+        const products = [
+        { nombrejuguete: "Casa grande", precio: 5000 },
+        { nombrejuguete: "Casa mediana", precio: 3500 },
+        { nombrejuguete: "Casa pequeña", precio: 2500 },
+        { nombrejuguete: "Pack juguetes", precio: 500 },
+        ];
+
+        selectedProduct = products.find(product => product.nombrejuguete === selectedProductName);
+
+        if (!selectedProduct) {
+         productoError.textContent = "El producto seleccionado no se encuentra en la lista.";
+            return;
+        }
+        productoError.textContent = "";
+        resultado.style.display = "block";
+        productoInfo.innerHTML = `
+        <h3>ID: ${selectedProduct.id}</h3>
+        <h3>Nombre: ${selectedProduct.nombrejuguete}</h3>
+        <h3>Precio: $${selectedProduct.precio}</h3>
+        `;
+        productoPrecio.style.display = "block";
+        productoPrecio.textContent = `Precio: $${selectedProduct.precio}`;
+    });
+
+    comprarBtn.addEventListener("click", function () {
+        compras.push(selectedProduct);
+        totalCost += selectedProduct.precio;
+        productoInfo.innerHTML = "";
+        productoPrecio.style.display = "none";
+
+        let compraDetails = `<p>Producto: ${selectedProduct.nombrejuguete} - Precio: $${selectedProduct.precio}</p>`;
+        comprasInfo.innerHTML += compraDetails;
+
+        if (confirm("¿Deseas agregar más productos al carrito?")) {
+        productoSelect.value = "Casa grande";
+            resultado.style.display = "none";
+        } else {
+            mostrarResumenCompra();
+        }
+    });
+
+    continuarComprandoBtn.addEventListener("click", function () {
+        productoSelect.value = "Casa grande";
+        resultado.style.display = "none";
+    });
+
+    function mostrarResumenCompra() {
+        compraSection.style.display = "block";
+        totalGasto.textContent = "$" + totalCost.toFixed(2);
+
+        compradorData.nombre = nombre;
+        compradorData.direccion = direccion;
+        compradorData.telefono = telefono;
+
+        localStorage.setItem(APP_PREFIX + "nombre", compradorData.nombre);
+        localStorage.setItem(APP_PREFIX + "direccion", compradorData.direccion);
+        localStorage.setItem(APP_PREFIX + "telefono", compradorData.telefono);
+
+        document.getElementById("nombreComprador").textContent = compradorData.nombre;
+        document.getElementById("direccionComprador").textContent = compradorData.direccion;
+        document.getElementById("telefonoComprador").textContent = compradorData.telefono;
+
+        const mensajeDespedida = `¡Gracias por su compra, ${compradorData.nombre}! Esperamos que disfrute de su juguete.`;
+        const mensajeDespedidaElement = document.createElement("p");
+        mensajeDespedidaElement.textContent = mensajeDespedida;
+        datosComprador.appendChild(mensajeDespedidaElement);
     }
-}
-
-if (compras.length > 0) {
-    let datosComprador = obtenerDatosComprador();
-
-    alert("Resumen de su compra:");
-    for (let compra of compras) {
-        alert(`Producto: ${compra.nombre} - Precio: $${compra.precio}`);
-    }
-
-    alert(`Total gastado: $${totalGasto}`);
-    alert(`Datos del comprador:\nNombre: ${datosComprador.nombre}\nDirección: ${datosComprador.direccion}\nTeléfono: ${datosComprador.telefono}`);
-} else {
-    alert("No se realizaron compras.");
-}
+});
